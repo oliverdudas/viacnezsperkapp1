9e17814ccbe958229cda4126d694501bce2ed101
OliverDudas <OliverDudas@gmail.com>
2012-10-28 14:07:14 +0000

    - authentication success handler

---
+++ src/main/java/sk/dudas/appengine/viacnezsperk/security/UserAdminAuthenticationSuccessHandler.java
@@ -0,0 +1,41 @@
+package sk.dudas.appengine.viacnezsperk.security;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import sk.dudas.appengine.viacnezsperk.domain.Role;
+import sk.dudas.appengine.viacnezsperk.domain.User;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Map;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: oli
+ * Date: 28.10.2012
+ * Time: 14:40
+ * To change this template use File | Settings | File Templates.
+ */
+public class UserAdminAuthenticationSuccessHandler implements AuthenticationSuccessHandler {
+
+    private Map<String, String> roleUrlMap;
+
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+        if (authentication.getPrincipal() instanceof UserDetails) {
+            User user = (User) authentication.getPrincipal();
+            if (user.hasRole(Role.ROLE_ADMIN)) {
+                response.sendRedirect(request.getContextPath() + roleUrlMap.get(Role.ROLE_ADMIN));
+            } else if (user.hasRole(Role.ROLE_USER)) {
+                response.sendRedirect(request.getContextPath() + roleUrlMap.get(Role.ROLE_USER));
+            }
+        }
+    }
+
+    public void setRoleUrlMap(Map<String, String> roleUrlMap) {
+        this.roleUrlMap = roleUrlMap;
+    }
+}
